48a043a05c5d9c8faae79d0666ea8c7fab4a584d
GioDots <dodedodo96@gmail.com>
2020-03-16 15:59:19 +0100

    cd

--- course-project/src/app/places/discover/discover.page.ts
+++ course-project/src/app/places/discover/discover.page.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { PlacesService } from '../places.service';
-import { Place } from '../place.model';
 import { MenuController } from '@ionic/angular';
+import { SegmentChangeEventDetail } from '@ionic/core'
+
+import { Place } from '../place.model';
 
 @Component({
   selector: 'app-discover',
@@ -11,13 +13,18 @@ import { MenuController } from '@ionic/angular';
 export class DiscoverPage implements OnInit {
 
   loadedPlaces: Place[];
+  listedLoadedPlaces: Place[];
 
   constructor(private placesService: PlacesService, private menuCtrl: MenuController) { }
 
   ngOnInit() {
     this.loadedPlaces = this.placesService.places;
+    this.listedLoadedPlaces = this.loadedPlaces.slice(1);
   }
 
+  onFilterUpdate(event: CustomEvent<SegmentChangeEventDetail>){
+    console.log(event.detail);
+  }
   // onOpenMenu(){
   //   this.menuCtrl.toggle();
   // }
